3a683d05c4f5dd94c9feec2f1fe0e12e00dde9c3
Abhi Arya <abhia9167@gmail.com>
2024-03-10 16:04:08 -0700

    feat: added executable to git

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,3 @@
 printf "installing pythonize to your machine... \nyou may be prompted to input a password...\n"
-./scripts/build.sh > /dev/null
 sudo mv pythonize /usr/local/bin 
-rm -r venv 
-rm requirements.txt 
 printf "installation complete! have fun! - abhi\n "
-deactivate
